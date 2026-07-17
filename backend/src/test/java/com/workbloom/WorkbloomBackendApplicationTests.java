2244370938cf29c0db0698ae37fd8ed3eed4a322
Manu1067 <vmanushree2006@gmail.com>
2026-07-17 22:21:47 +0530

    Intial project setup

---
+++ backend/src/test/java/com/workbloom/WorkbloomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbloomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
